e6f025eacbfa6bebca903f9dbb6083d314d1c9a4
Artur Geghamyan <artur.geghamyan@exio.tech>
2021-12-15 17:17:02 +0400

    User CRUD

---
+++ src/user/controller/user.controller.ts
@@ -0,0 +1,35 @@
+import { User } from './../models/user.interface';
+import { UserService } from './../service/user.service';
+import { Body, Controller, Post, Get, Param, Delete, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Controller('users')
+export class UserController {
+
+    constructor(private userService: UserService) { }
+
+    @Post()
+    create(@Body() user: User): Observable<User> {
+        return this.userService.create(user)
+    }
+
+    @Get(':id')
+    findOne(@Param() params): Observable<User> {
+        return this.userService.findOne(params.id)
+    }
+
+    @Get()
+    findAll(): Observable<User[]> {
+        return this.userService.findAll()
+    }
+
+    @Delete(':id')
+    deleteOne(@Param() params): Observable<User> {
+        return this.userService.deleteOne(params.id)
+    }
+
+    @Put(':id')
+    updateOne(@Param('id') id: string, @Body()user: User): Observable<any> {
+        return this.userService.updateOne(Number(id), user)
+    }
+}
